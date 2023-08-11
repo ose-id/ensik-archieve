408c7f412db5c306ba6edb5c358da6a82bf50e1e
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-11 20:10:30 +0700

    modules: add supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["@nuxtjs/supabase"],
+
+  devtools: { enabled: true },
+});
