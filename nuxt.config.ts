c5949f1ea663e2cc9c73cc8610572e286c246ec0
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-12 11:22:35 +0700

    modules: add tailwind css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,12 @@ export default defineNuxtConfig({
   modules: ["@nuxtjs/supabase"],
 
   devtools: { enabled: true },
+
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 });
