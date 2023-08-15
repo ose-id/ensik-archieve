8999c66392e965eb2a2cb9973fda4a704aca204f
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-15 23:25:37 +0700

    docs: fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  darkMode: "class",
-  content: ["./components/**/*.{vue,js}", "./layouts/**/*.vue", "./pages/**/*.vue", "./plugins/**/*.{js,ts}", "./nuxt.config.{js,ts}", "./app.vue"],
+  darkMode: 'class',
+  content: [
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
+    "./app.vue"
+  ],
   theme: {
-<<<<<<< HEAD
     extend: {
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", ...defaultTheme.fontFamily.sans]
       },
-=======
-    extend: {},
-    fontFamily:{
-      Poppins: ['Poppins'],
->>>>>>> main
     },
   },
   plugins: [],
-};
+}
+
