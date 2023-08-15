5dde327ea7e671f215791f80674a35816f1d6d7c
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-15 16:29:19 +0700

    docs: update

--- pages/login.vue
+++ pages/login.vue
@@ -1,3 +1,4 @@
 <template>
-  <NuxtWelcome />
+  <h1 class="font-bold">Login Disini</h1>
+  <NuxtLink to="/register" class="hover:text-blue-500">To Register</NuxtLink>
 </template>
---
+++ pages/register.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1 class="font-bold">Register Disini</h1>
+  <NuxtLink to="/login" class="hover:text-blue-500">To Login</NuxtLink>
+</template>
