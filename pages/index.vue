d698a35d7f6497a1e40f319da7c448f4908549d3
Tanuki74 <bannyjuanda@gmail.com>
2023-08-15 21:08:13 +0700

    Menambahkan link login dan register di file index

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
 <template>
-  <NuxtWelcome />
+    <h1 class="font-bold">Login Disini</h1>
+    <NuxtLink to="/register" class="hover:text-blue-500">To Register</NuxtLink>
+    <br />
+    <h1 class="font-bold">Register Disini</h1>
+    <NuxtLink to="/login" class="hover:text-blue-500">To Login</NuxtLink>
 </template>
