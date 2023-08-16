460a24b5d0f4aad2dc1afeedb79735d5493a935a
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-16 11:29:27 +0700

    docs: create new components

---
+++ components/nav.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="flex justify-center">
+    <ul class="flex space-x-6">
+      <li>
+        <NuxtLink to="/register" class="hover:text-blue-500">REGISTER</NuxtLink>
+      </li>
+      <li><NuxtLink to="/login" class="hover:text-blue-500">LOGIN</NuxtLink></li>
+    </ul>
+  </nav>
+</template>
