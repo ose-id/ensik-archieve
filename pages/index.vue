b9787eca9c495251202bf2eff0e7d9c58dc399db
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-16 11:28:18 +0700

    docs: add seo meta

--- pages/index.vue
+++ pages/index.vue
@@ -24,3 +24,10 @@
     </div>
   </main>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: "Ensik Archieve",
+  description: "What is the Ensik Archive? To save some ensikolopedia member photos",
+});
+</script>
--- pages/login.vue
+++ pages/login.vue
@@ -2,3 +2,10 @@
   <h1 class="font-bold">Login Disini</h1>
   <NuxtLink to="/register" class="hover:text-blue-500">To Register</NuxtLink>
 </template>
+
+<script lang="ts" setup>
+useSeoMeta({
+  title: "Login - Ensik Archieve",
+  description: "What is the Ensik Archive? To save some ensikolopedia member photos",
+});
+</script>
