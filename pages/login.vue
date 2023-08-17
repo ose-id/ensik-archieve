df6e5e3d4089766ea43cca7328c29153f50eb27e
Tanuki74 <bannyjuanda@gmail.com>
2023-08-17 20:24:11 +0700

    chore: update style login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,13 +1,25 @@
 <template>
-    <div class="grid grid-cols-2">
-        <div class="bg-[#146C94]">
-            
-        </div>
-        <div>
-            <section class="bg-white">
-                <h1 class="font-bold">Login</h1>
-                <NuxtLink to="/register" class="hover:text-[#146C94] text-[#19A7CE]">Register</NuxtLink>
-            </section>
+    <div class="container mx-auto">
+        <div class="grid grid-cols-2 ">
+            <div class="bg-[#146C94] w-full">
+
+            </div>
+            <div class="w-full">
+                <section class="bg-white pt-7">
+                    <h1 class="font-bold uppercase text-5xl tracking-[20px] text-b text-center">Login</h1>
+                    <form>
+                        <label class="block">
+                            <span class="block text-sm">Username</span>
+                            <input type="text" value="" class="mt-1 w-auto px-3 py-0.5 bg-white border border-slate-900 rounded-sm" />
+                        </label>
+                        <label class="block mt-6">
+                            <span class="block text-sm">Password</span>
+                            <input type="password" value="" class="mt-1 w-auto px-3 py-0.5 bg-white border border-slate-900 rounded-sm" />
+                        </label>
+                    </form>
+                    <NuxtLink to="/register" class="hover:text-[#146C94] text-[#19A7CE]">Register</NuxtLink>
+                </section>
+            </div>
         </div>
     </div>
 </template>
--- pages/register.vue
+++ pages/register.vue
@@ -1,6 +1,6 @@
 <template>
   <main class="bg-[#146C94] w-full pt-10 font-Poppins">
-    <h1 class="font-bold uppercase text-5xl tracking-[20px] text-white text-center">register</h1>
+    <h1 class="font-bold uppercase text-5xl tracking-[20px] text-white text-center">Register</h1>
     <section class="w-full h-[50%] rounded-t-[50px] bg-white mt-10 p-20">
       <!-- Lanjut besok -->
     </section>
