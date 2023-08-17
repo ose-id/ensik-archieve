e3d6e2f28491847dc57817ee2e5c206bb314dd90
Tanuki74 <bannyjuanda@gmail.com>
2023-08-17 19:21:58 +0700

    commit login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,4 +1,13 @@
 <template>
-  <h1 class="font-bold">Login Disini</h1>
-  <NuxtLink to="/register" class="hover:text-blue-500">To Register</NuxtLink>
+    <div class="grid grid-cols-2">
+        <div class="bg-[#146C94]">
+            
+        </div>
+        <div>
+            <section class="bg-white">
+                <h1 class="font-bold">Login</h1>
+                <NuxtLink to="/register" class="hover:text-[#146C94] text-[#19A7CE]">Register</NuxtLink>
+            </section>
+        </div>
+    </div>
 </template>
