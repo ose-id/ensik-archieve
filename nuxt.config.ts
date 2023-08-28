280bf19553f7ca0d048fec32022f7d09060410e8
devdewa <uchiasasuke123.sp@gmail.com>
2023-08-28 11:49:33 +0700

    perf: update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      htmlAttrs: {
+        lang: "en",
+      },
+    },
+  },
+  
   modules: [
     // "@nuxtjs/supabase",
     "@nuxtjs/google-fonts",
