2f96838372fc84a5819a54ff8c6e521c1b74d515
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-02 05:54:33 +0700

    refactor(style): change code style

--- components/card.vue
+++ components/card.vue
@@ -1,21 +1,10 @@
 <template>
-  <nuxt-img v-for="item in img" :class="imgStyle" :src="item.i" loading="lazy" alt="img" />
+  <nuxt-img v-for="item in img" :key="item.i" :src="item.i" :class="imgStyle" loading="lazy" alt="img" placeholder />
 </template>
 
-<script lang="ts" setup>
+<script setup>
 const imgStyle = "h-auto max-w-full rounded-lg";
-const img = [
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-1.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-2.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-5.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-6.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-7.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-8.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-9.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-10.jpg" },
-  { i: "https://flowbite.s3.amazonaws.com/docs/gallery/square/image-11.jpg" },
-];
+const img = Array.from({ length: 12 }, (_, i) => ({
+  i: i === 0 ? `https://flowbite.s3.amazonaws.com/docs/gallery/square/image.jpg` : `https://flowbite.s3.amazonaws.com/docs/gallery/square/image-${i}.jpg`,
+}));
 </script>
