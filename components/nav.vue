aeae9d944e136b02f12ec00b05ffe0e6b02540ff
devdewa <uchiasasuke123.sp@gmail.com>
2023-09-07 09:03:09 +0700

    chore: remove name & add profile and post list

--- components/nav.vue
+++ components/nav.vue
@@ -2,10 +2,13 @@
   <nav class="flex justify-center">
     <ul class="flex space-x-6" v-if="user">
       <li>
-        {{ user.name }}
+        <NuxtLink to="/profile" class="hover:text-blue-500">Profile</NuxtLink>
       </li>
       <li>
-        <button @click="userLogout">Logout</button>
+        <NuxtLink to="/post" class="hover:text-blue-500">Post</NuxtLink>
+      </li>
+      <li>
+        <button @click="userLogout" class="text-red-400">Logout</button>
       </li>
     </ul>
     <ul class="flex space-x-6" v-else>
@@ -19,7 +22,26 @@
 
 <script setup>
 const { auth } = useSupabaseClient();
+const supabase = useSupabaseClient();
 const user = useSupabaseUser();
+const userProfile = ref(null);
+
+async function fetchUserProfile() {
+  if (user.value) {
+    const { data, error } = await supabase.from("profiles").select("name").eq("email", user.value.email);
+
+    if (error) {
+      alert(error.message);
+    } else {
+      userProfile.value = data[0];
+    }
+  }
+}
+
+onMounted(() => {
+  fetchUserProfile();
+});
+
 const userLogout = async () => {
   await auth.signOut();
 };
