15fd9c232fe5b84ffc0b81bd771a9be771ee4299
devdewa <uchiasasuke123.sp@gmail.com>
2024-07-09 05:16:02 +0700

    feat: use middleware from nuxt auth utils

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,9 @@
-export default defineNuxtRouteMiddleware(() => {
-  const user = useSupabaseUser();
-  if (!user.value)
-    return navigateTo('/login');
+export default defineNuxtRouteMiddleware(async (to, _from) => {
+  const session = await useUserSession();
+
+  const protectedRoutes = ['/'];
+
+  if (!session?.user && protectedRoutes.includes(to.path)) {
+    return navigateTo('/auth/discord');
+  }
 });
