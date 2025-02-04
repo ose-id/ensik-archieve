ef3bb00ee57f95f6e631047ba532bbabf9e60d75
adydetra <devdewa123.sp@gmail.com>
2025-02-05 00:57:16 +0700

    chore: lint

--- components/ImageUpload.vue
+++ components/ImageUpload.vue
@@ -66,7 +66,7 @@ const closeModal = () => {
           :src="imageUrl"
           :alt="discordUser?.username || 'Uploaded Image'"
           class="h-16 w-16 object-cover rounded-full mr-6"
-          @click="showModal = true" 
+          @click="showModal = true"
         >
       </div>
       <label class="block">
@@ -89,34 +89,57 @@ const closeModal = () => {
       Uploading...
     </p>
 
-    <div v-if="showModal" class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50">
+    <div
+      v-if="showModal"
+      class="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50"
+    >
       <!-- Modal Container -->
       <div class="relative bg-white rounded-lg shadow-lg w-11/12 md:w-1/2 lg:w-1/3">
         <!-- Header -->
         <div class="px-6 py-4 border-b border-gray-200 flex justify-between items-center">
-          <h2 class="text-xl font-semibold text-gray-800">Preview Image</h2>
-          <button @click="closeModal"
-            class="absolute top-3 right-3 p-2 bg-gray-100 hover:bg-gray-200 transition duration-300 rounded-full text-gray-600 hover:text-gray-800">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+          <h2 class="text-xl font-semibold text-gray-800">
+            Preview Image
+          </h2>
+          <button
+            class="absolute top-3 right-3 p-2 bg-gray-100 hover:bg-gray-200 transition duration-300 rounded-full text-gray-600 hover:text-gray-800"
+            @click="closeModal"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
             </svg>
           </button>
         </div>
 
         <!-- Content -->
         <div class="px-6 py-4 flex justify-center items-center">
-          <img :src="imageUrl" alt="Gambar yang diunggah" class="max-w-full max-h-96 rounded-lg mx-auto">
+          <img
+            :src="imageUrl"
+            alt="Gambar yang diunggah"
+            class="max-w-full max-h-96 rounded-lg mx-auto"
+          >
         </div>
 
         <!-- Footer -->
         <div class="px-6 py-4 border-t border-gray-200 flex justify-center">
-          <button @click="uploadImage"
-            class="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 transform hover:scale-105">
+          <button
+            class="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-semibold py-3 px-6 rounded-lg shadow-md transition duration-300 transform hover:scale-105"
+            @click="uploadImage"
+          >
             Upload
           </button>
         </div>
       </div>
     </div>
-
   </div>
 </template>
