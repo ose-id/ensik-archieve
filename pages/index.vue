a52a8494dcb09fc0d07dabe6b023432c9faa9ace
adydetra <devdewa123.sp@gmail.com>
2025-02-04 10:20:50 +0700

    chore: use user session from nuxt auth utils

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
-<script setup>
+<script setup lang="ts">
 useSeoMeta({
   title: 'Ensik Archieve',
 });
+
+const { loggedIn } = useUserSession();
 </script>
 
 <template>
   <section class="container mx-auto py-12">
     <TheNav />
+    <div
+      v-if="loggedIn"
+      class="mt-8"
+    >
+      <ImageUpload />
+    </div>
+    <div
+      v-else
+      class="text-center text-gray-500 mt-8"
+    >
+      <p>Login with Discord to upload images.</p>
+    </div>
     <ImageGallery />
-    <ImageUpload />
   </section>
 </template>
