20dbad6d30d148f8effe5b98b068df26e7f0ffcd
adydetra <devdewa123.sp@gmail.com>
2025-02-06 10:56:01 +0700

    chore: change favicon icon / logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,18 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en',
       },
+      meta: [
+        { name: 'keywords', content: 'Ensik Archive is a captivating online gallery platform dedicated to preserving and showcasing a rich collection of historical, and documents.' },
+        { name: 'author', content: 'OSE' },
+        { name: 'apple-mobile-web-app-title', content: 'Ensik Arch' },
+      ],
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
+        { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/favicon/site.webmanifest' },
+      ],
     },
   },
 
