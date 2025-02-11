ffdc258b6298f18cfc0e79fc5e12e25a00663548
adydetra <devdewa123.sp@gmail.com>
2025-02-11 14:31:28 +0700

    chore: remove unused tag

--- components/TheSide.vue
+++ components/TheSide.vue
@@ -11,26 +11,24 @@ const menuItems: MenuItem[] = [
 </script>
 
 <template>
-  <aside class="fixed left-0 z-50 min-h-screen w-12 border-r-1 border-transparent border-r-solid bg-white shadow-lg md:w-20 dark:border-gray-800/70 dark:bg-gray-950">
-    <div flex flex-col h-dvh>
-      <div flexcenter px-4 py-6>
-        <NuxtImg src="/img/logo.jpg" alt="Logo" size-6 rounded-full md:size-8 />
-      </div>
+  <aside class="fixed left-0 z-50 min-h-screen w-12 flex flex-col border-r-1 border-transparent border-r-solid bg-white shadow-lg md:w-20 dark:border-gray-800/70 dark:bg-gray-950">
+    <div flexcenter px-4 py-6>
+      <NuxtImg src="/img/logo.jpg" alt="Logo" size-6 rounded-full md:size-8 />
+    </div>
 
-      <!-- Navigation Menu -->
-      <nav flex-grow>
-        <ul p-2 space-y-6>
-          <li v-for="item in menuItems" :key="item.label">
-            <NuxtLink :to="item.route" flexcenter rounded-lg p-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800>
-              <div text-2xl :class="[item.icon]" />
-            </NuxtLink>
-          </li>
-        </ul>
-      </nav>
+    <!-- Navigation Menu -->
+    <nav flex-grow>
+      <ul p-2 space-y-6>
+        <li v-for="item in menuItems" :key="item.label">
+          <NuxtLink :to="item.route" flexcenter rounded-lg p-2 transition-colors duration-200 hover:bg-gray-100 dark:hover:bg-gray-800>
+            <div text-2xl :class="[item.icon]" />
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
 
-      <div flexcenter px-4 py-6>
-        <ColorMode />
-      </div>
+    <div flexcenter px-4 py-6>
+      <ColorMode />
     </div>
   </aside>
 </template>
