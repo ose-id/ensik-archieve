fda50f616caf10cbe45494019749c5801516e9d6
adydetra <devdewa123.sp@gmail.com>
2025-07-23 22:54:29 +0700

    chore(deps): upgrade to nuxt 4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -28,7 +28,7 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/png', href: '/favicon/favicon-96x96.png', sizes: '96x96' },
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon/favicon.svg' },
-        { rel: 'shortcut icon', href: '/favicon/favicon.ico' },
+        { rel: 'shortcut icon', href: 'favicon.ico' },
         { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
         { rel: 'manifest', href: '/favicon/site.webmanifest' },
       ],
